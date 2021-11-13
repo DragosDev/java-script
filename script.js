0cb5317654fdfca02a3caf375a87017f785b6d92
DragosDev <dragosmotrea0@gmail.com>
2021-11-13 08:58:49 +0200

    added dark mode

--- script.js
+++ script.js
@@ -1,16 +1,79 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const toggleIcon = document.getElementById('toggle-icon');
+
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
+// Dark or Light Images
+function imageMode(color) {
+    
+}
+
+
+//Toggle Switch Function
+function toggleDarkLightMode(isDark){
+  
+    toggleIcon.children[0].textContent= isDark ? "Dark Mode" : "Light Mode" ;
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun','fa-moon') :
+      toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
+    isDark ? imageMode('dark') : imageMode('light');
+}
+
+//Switch Theme Dynamically
+function switchTheme(event){
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        toggleDarkLightMode(true);
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        toggleDarkLightMode(false);
+    }
+}
+
+//Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
+
+//Check local storage for theme changes
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        toggleDarkLightMode(true);
+    }
+}
+
+
+
+
 //Unsplash API
-const count = 10;
+const count = 30;
 const apiKey ='adMDW8ghSMGg68upthQ1ABugV0RlSmH3tQcuWLBcZmc';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Check if images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages){
+        ready = true;
+        loader.hidden = true;
+    }
+
+}
 
 //Create Elements For Links & Photos , Add to the document
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+
     //Function for each method
     photosArray.forEach((photo) => {
         //Create an anchor element to link to Unsplash API
@@ -20,14 +83,15 @@ function displayPhotos() {
         // Create image for photo
         const img = document.createElement('img');
         img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt',photo.alt_description);
+        img.setAttribute('name', photo.user.name);
+        img.setAttribute('alt',photo.username);
         img.setAttribute('title',photo.alt_title);
-        //Put the img inside the a, then put both inside the imageContainer element
+  
+        //Put the img inside the a, then put both inside the imageContainer 
+
+        img.addEventListener('load', imageLoaded);
         item.appendChild(img);
         imageContainer.appendChild(item);
-
-
-
     });
 }
 
@@ -42,6 +106,15 @@ async function getPhotos(){
         //Catches error
     }
 }
+
+//Check to see if scrolling is near the bottom of the page
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();  
+    }
+
+});
 
 // On Load
 getPhotos();
